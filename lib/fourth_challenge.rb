d79ae58abb3b17d79e314e1d0883757165d00826
Denali Balser <denali.balser167@gmail.com>
2019-04-03 13:26:12 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,6 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         { :name => "Benvolio", :age => "17", :attitude => "worried"}
+        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
       ]
    },
    :capulet => {
@@ -13,7 +14,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {:name => , :age => , :attitude => }
+        {:name => "", :age => , :attitude => }
       ]
    }
 }
